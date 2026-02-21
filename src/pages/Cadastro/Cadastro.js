ddbabb0205aea323976d0ab659290c3cf9a70bd4
Samu Mene <samuelsantiago2222@gmail.com>
2026-02-21 19:39:57 -0300

    estrutura de layout do cadastro

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,9 +1,14 @@
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
 
 export default function Cadastro() {
   return (
-    <View>
-      <Text className="text-2xl">Tela de Cadastro</Text>
+    <View style={{ flex: 1, flexDirection: "column" }}>
+      {/* IMAGEM DE FUNDO */}
+      <View style={{ flex: 0.2 }} className="bg-slate-900 rounded-b-2xl"></View>
+
+      <View style={{flex:0.8}} className="bg-red-200">
+
+      </View>
     </View>
   );
 }
