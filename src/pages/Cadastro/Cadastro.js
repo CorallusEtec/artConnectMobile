4f035afff08187093ce512705680fc71c3145a11
Samu Mene <samuelsantiago2222@gmail.com>
2026-02-21 23:37:07 -0300

    Layout pronto tela de cadastro

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,7 +1,9 @@
 import { View, Image, Text, TextInput, Pressable } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 export default function Cadastro() {
+  const navigation = useNavigation();
   const meses = [
     { num: 1, nome: "Janeiro" },
     { num: 2, nome: "Fevereiro" },
@@ -24,11 +26,10 @@ export default function Cadastro() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   
-  
   return (
     <View style={{ flex: 1, flexDirection: "column" }}>
       {/* IMAGEM DE FUNDO */}
-      <View style={{ flex: 0.15 }}>
+      <View style={{ flex: 0.1 }}>
         <Image
           className="rounded-b-2xl"
           style={{ maxHeight: "100%", maxWidth: "100%" }}
@@ -36,8 +37,8 @@ export default function Cadastro() {
         />
       </View>
 
-      <View style={{ flex: 0.8, flexDirection: "column" }} className="m-2">
-        <View className="items-center">
+      <View style={{ flex: 0.8, flexDirection: "column" }}>
+        <View className="items-center mb-2">
           <Text className="text-2xl font-semibold text-emerald-700">
             FAÇA SEU CADASTRO
           </Text>
@@ -57,7 +58,7 @@ export default function Cadastro() {
           />
         </View>
 
-        <View style={{flex:0.14}} className="m-3">
+        <View style={{flex:0.16}} className="m-3">
           <View style={{flex:1}} className="flex-row gap-2 ">
             <Image
               style={{ height: 40, width: 40 }}
@@ -76,10 +77,10 @@ export default function Cadastro() {
               keyboardType="numeric"
               value={dia}
               onChangeText={(e) => setDia(e)}
-              className="placeholder:text-green-200 outline-none text-center font-semibold bg-emerald-500 text-lg p-3 w-1/4 text-white rounded-full"
+              className="placeholder:text-green-200 outline-none text-center font-semibold bg-emerald-500 text-md p-1 w-1/4 text-white rounded-full"
             />
             <Picker
-              className="bg-emerald-500 p-3 text-lg text-white rounded-full font-semibold text-center"
+              className="bg-emerald-500 p-2 pr-3 text-md text-white rounded-full font-semibold text-center"
               selectedValue={mes}
               onValueChange={(mes) => setMes(mes)}
             >
@@ -96,12 +97,12 @@ export default function Cadastro() {
               inputMode="numeric"
               maxLength={4}
               keyboardType="numeric"
-              className="placeholder:text-green-200 outline-none text-lg text-center font-semibold bg-emerald-500 p-3 w-1/4 text-white rounded-full"
+              className="placeholder:text-green-200 outline-none text-md text-center font-semibold bg-emerald-500 p-1 w-1/4 text-white rounded-full"
             />
           </View>
         </View>
         
-        <View style={{flex:0.1}} className="mb-5 flex-row items-center border-emerald-700 border-b-2 gap-2 pb-2 ml-3 mr-3">
+        <View className="mb-5 flex-row items-center border-emerald-700 border-b-2 gap-2 pb-2 ml-3 mr-3">
           <Image source={require("bootstrap-icons/icons/envelope-fill.svg")}
           tintColor="#009966"
           style={{height:40, width:40}}
@@ -137,9 +138,12 @@ export default function Cadastro() {
             source={mostrarSenha?require('bootstrap-icons/icons/eye-fill.svg'):require('bootstrap-icons/icons/eye-slash-fill.svg')} />
           </Pressable>
         </View>
-        <View className="flex-row justify-center">
+        <View className="flex-row items-baseline justify-around mb-5 mr-5">
           <Pressable className="  bg-emerald-500  p-3  rounded-full">
-            <Text className="text-lg text-white text-center font-semibold">Criar conta</Text>
+            <Text className="text-md text-white text-center font-semibold">Criar conta</Text>
+          </Pressable>
+          <Pressable onPress={()=>navigation.navigate("Login")}>
+            <Text className="text-emerald-700 font-semibold">Já tem uma Conta? Faça Login</Text>
           </Pressable>
         </View>
       </View>
