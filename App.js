1c07bc68311fab7fae658302622cb05ba0c711d2
Lucas Pereira <lucaspereiravarjao83@gmail.com>
2026-03-03 08:41:51 -0300

    TelaEsqueciSenha

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./src/pages/Login/Login";
 import Cadastro from "./src/pages/Cadastro/Cadastro";
+import EsqueciSenha from "./src/pages/EsqueciSenha/EsqueciSenha";
 import { NavigationContainer } from "@react-navigation/native";
 import "./global.css";
 
@@ -11,6 +12,7 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Cadastro" component={Cadastro} options={{headerShown: false}} />
           <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+          <Stack.Screen name="EsqueciSenha" component={EsqueciSenha} options={{headerShown: false}} />
         </Stack.Navigator>
       </NavigationContainer>
   );
