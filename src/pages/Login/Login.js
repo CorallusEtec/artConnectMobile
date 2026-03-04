9ca055a9105711638c19d08fa3f4bee59fff48cf
Lucas-Pereira-2008 <lucaspereiravarjao2008@gmail.com>
2026-03-04 14:56:53 -0300

    Reboot tela da login Não Finalizado

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,74 +1,90 @@
-
 import { View, Text, Pressable, Image } from "react-native";
 import { TextInput } from "react-native-web";
 import { useNavigation } from "@react-navigation/native";
 
+{/*SE VOCê ESTÁ LENDO ISSO, O COMMIT DEU CERTO!*/}
+
 export default function Login() {
     const navigation = useNavigation();
 
+
     return (
-        <View style={{ flex: 1, flexDirection: "column" }}>
+        <View style={{ flex: 1, flexDirection: "column", backgroundColor: "#04CBAC" }}>
+
 
             {/*IMAGEM DE FUNDO*/}
-            <View style={{ flex: 0.1 }}>
-                <Image
-                className="rounded-b-2xl"
-                style={{ maxHeight: "100%", maxWidth: "100%" }}
-                source={require("../../../assets/img/banner.jpeg")}
-                />
+            <View style={{ flex: 0.15, maxHeight: "100%", maxWidth: "100%", backgroundColor: "#04CBAC" }}>
             </View>
+
 
             {/*TÍTULO*/}
-            <View className="items-center mb-2">
-            <Text className="text-2xl font-semibold text-emerald-700">
-                FAÇA SEU LOGIN
-            </Text>
-            </View>
+            <View style={{ flex: 0.9, flexDirection: "column", borderTopLeftRadius:30, borderTopRightRadius:30, backgroundColor: "#ffffff" }}>
+                <View className="items-center mb-2">
+                    <Text className="text-5xl text-gray-600 m-2.5">
+                        Login
+                    </Text>
 
-            {/*E-MAIL*/}
-            <View className="flex-row items-center border-emerald-700 border-b-2 pb-2 ml-3 mr-3 mt-6 gap-5">
-                <Image source={require("bootstrap-icons/icons/envelope-fill.svg")}
-                tintColor="#009966"
-                style={{height:40, width:40}}
-                />
-                <TextInput
-                    className="w-[90%] outline-none text-xl text-emerald-700 font-semibold placeholder:text-emerald-400"
-                    placeholder="E-mail"
-                />
-            </View>
 
-            {/*SENHA*/}
-            <View className="flex-row items-center border-emerald-700 border-b-2 pb-2 ml-3 mr-3 mt-6 gap-5">
-                <Image source={require("bootstrap-icons/icons/lock-fill.svg")}
-                tintColor="#009966"
-                style={{height:40, width:40}}
-                />
-                <TextInput
-                    className="w-[90%] outline-none text-xl text-emerald-700 font-semibold placeholder:text-emerald-400"
-                    placeholder="Senha"
-                />
-                <Image source={require("bootstrap-icons/icons/eye-fill.svg")}
-                tintColor="#009966"
-                style={{height:40, width:40}}
-                />
-            </View>
+                    <Text className="text-xl text-gray-500 mb-8">
+                        Bem vindo! Entre com sua conta.
+                    </Text>
+                </View>
 
-            <View className="flex-row items-center mt-6">
-                <Text className="font-semibold w-30 mx-10">Lembrar Login</Text>
-                <Pressable><Text className="text-emerald-700 font-semibold w-30 mx-10">Esqueci a Senha</Text></Pressable>
-            </View>
 
-            {/*ENTRAR*/}
-            <View className="items-center mt-6">
-                <Pressable className="bg-emerald-500  p-3  rounded-full w-1/4">
-                <Text className="text-md text-white text-center font-semibold">Entrar</Text>
-                </Pressable>
-            </View>
+                {/*E-MAIL*/}
+                <View className="flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg ml-3 mr-3 mt-6 gap-2">
+                    <Image source={require("bootstrap-icons/icons/envelope.svg")}
+                    tintColor="#5a5a5a"
+                    style={{height:25, width:25, margin:7}}
+                    />
+                    <TextInput
+                        className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
+                        placeholder="E-mail"
+                    />
+                </View>
 
-            {/*CADASTRO*/}
-            <View className="flex-row items-center mt-6">
-                <Text className="font-semibold mx-10">Não tem Cadastro?</Text>
-                <Pressable onPress={()=>navigation.navigate("Cadastro")} ><Text className="text-emerald-700 font-semibold mx-10">Cadastre-se</Text></Pressable>
+
+                {/*SENHA*/}
+                <View className="flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg ml-3 mr-3 mt-6 gap-2">
+                    <Image source={require("bootstrap-icons/icons/lock.svg")}
+                    tintColor="#5a5a5a"
+                    style={{height:25, width:25, margin:7}}
+                    />
+                    <TextInput
+                        className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
+                        placeholder="Senha"
+                    />
+                    <Image source={require("bootstrap-icons/icons/eye.svg")}
+                    tintColor="#5a5a5a"
+                    style={{height:25, width:25, margin:7}}
+                    />
+                </View>
+
+
+                <View className="flex-row items-center mt-6">
+                    <Text className="font-semibold w-30 mx-10">Lembrar Login</Text>
+                    <Pressable><Text className="text-gray-500 w-30 mx-10">Esqueci a Senha</Text></Pressable>
+                </View>
+
+
+                {/*ENTRAR*/}
+                <View className="items-center mt-6">
+                    <Pressable style={{backgroundColor: "#04CBAC"}} className="mt-10 rounded-lg bg-emerald-500 p-3 w-2/4">
+                    <Text className="text-2xl text-white text-center">Logar</Text>
+                    </Pressable>
+                </View>
+                
+                <View className="items-center mt-6 mb-10">
+                    <Pressable style={{backgroundColor: "#fff"}} className="mt-10 border-gray-300 border-2 rounded-lg bg-emerald-500 p-3 w-3/4">
+                    <Text className="text-2xl text-gray-500 text-center">Entrar sem Login</Text>
+                    </Pressable>
+                </View>
+
+                {/*CADASTRO*/}
+                <View className="w-3/4 flex-row items-center mt-6">
+                    <Text className="font-semibold">Não tem Cadastro? </Text>
+                    <Pressable onPress={()=>navigation.navigate("Cadastro")} ><Text className="text-gray-500">Cadastre-se</Text></Pressable>
+                </View>
             </View>
         </View>
     );
