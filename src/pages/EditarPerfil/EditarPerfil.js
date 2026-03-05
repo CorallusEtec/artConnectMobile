230e4e8849befe4efe0c80bf2f9e3fcdde3c0f1e
Lucas-Pereira-2008 <lucaspereiravarjao2008@gmail.com>
2026-03-05 17:21:39 -0300

    Tela de Editar Perfil

--- src/pages/EditarPerfil/EditarPerfil.js
+++ src/pages/EditarPerfil/EditarPerfil.js
@@ -1,106 +1,141 @@
 import { View, Text, Pressable, Image } from "react-native";
 import { TextInput } from "react-native-web";
 import { useNavigation } from "@react-navigation/native";
-import Feather from '@expo/vector-icons/Feather';
-import { Checkbox } from 'expo-checkbox';
 import { useState } from "react";
-
-
-{/*SE VOCê ESTÁ LENDO ISSO, O COMMIT DEU CERTO!*/}
-
+import {Picker} from '@react-native-picker/picker';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Feather from '@expo/vector-icons/Feather';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 
 export default function EditarPerfil() {
     const navigation = useNavigation();
     const [isChecked, setChecked] = useState(false);
+    const [selectedUf, setSelectedUf] = useState();
 
 
     return (
         <View style={{ flex: 1, flexDirection: "column", backgroundColor: "#04CBAC" }}>
 
 
-
-
-            {/*IMAGEM DE FUNDO*/}
-            <View style={{ flex: 0.15, maxHeight: "100%", maxWidth: "100%", backgroundColor: "#04CBAC" }}>
+            {/*HEADER*/}
+            <View className="flex-row items-center ml-5" style={{ flex: 0.11, maxHeight: "100%", maxWidth: "100%", backgroundColor: "#04CBAC" }}>
+                <AntDesign name="arrow-left" size={24} color="white" />
+                <Text className="text-2xl text-gray-100 m-4">
+                    Editar Perfil
+                </Text>
             </View>
 
 
+            <View style={{ flex: 1.4, flexDirection: "column", backgroundColor: "#ffffff" }}>
+                
 
-
-            {/*TÍTULO*/}
-            <View style={{ flex: 0.9, flexDirection: "column", borderTopLeftRadius:30, borderTopRightRadius:30, backgroundColor: "#ffffff" }}>
+                {/*FOTO DE PERFIL*/}
                 <View className="items-center mb-2">
-                    <Text className="text-5xl text-gray-600 m-2.5">
-                        EDITAR PERFIL
-                    </Text>
-
-
-
-
-                    <Text className="text-xl text-gray-500 mb-8">
-                        Bem vindo! Entre com sua conta.
-                    </Text>
+                    {/*ÍCONE PROVISÓRIO*/}
+                    <MaterialCommunityIcons name="face-man-profile" size={125} color="black" />
                 </View>
-
-
-
-
-                {/*E-MAIL*/}
-                <View className="flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg ml-3 mr-3 mt-6 gap-2">
-                    <Feather style={{margin:7}} name="mail" size={25} color="#5a5a5a" />
-                    <TextInput
-                        className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
-                        placeholder="E-mail"
-                    />
-                </View>
-
-
-
-
-                {/*SENHA*/}
-                <View className="flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg ml-3 mr-3 mt-6 gap-2">
-                    <Feather style={{margin:7}} name="lock" size={25} color="#5a5a5a" />
-                    <TextInput
-                        className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
-                        placeholder="Senha"
-                    />
-                    <Feather style={{margin:7}} name="eye" size={25} color="#5a5a5a" />
-                </View>
-
-
-                <View className="flex-row items-center mt-6">
-                    <Checkbox
-                        className="ml-14"
-                        value={isChecked}
-                        onValueChange={setChecked}
-                        color={isChecked ? '#4630EB' : undefined}
-                    />
-                    <Text className="font-semibold w-30 mx-3">Lembrar Login</Text>
-                    <Pressable><Text className="text-gray-500 w-30 mx-14">Esqueci a Senha</Text></Pressable>
-                </View>
-
-
-
-
-                {/*ENTRAR*/}
-                <View className="items-center mt-6">
-                    <Pressable style={{backgroundColor: "#04CBAC"}} className="mt-10 rounded-lg bg-emerald-500 p-3 w-2/4">
-                    <Text className="text-2xl text-white text-center">Logar</Text>
+                <View className="flex-row items-center mb-2">
+                    <Pressable>
+                        <FontAwesome6 className="ml-36" name="trash-can" size={24} color="#04CBAC" />
                     </Pressable>
-                </View>
-               
-                <View className="items-center mt-6 mb-10">
-                    <Pressable style={{backgroundColor: "#fff"}} className="mt-10 border-gray-300 border-2 rounded-lg bg-emerald-500 p-3 w-3/4">
-                    <Text className="text-2xl text-gray-500 text-center">Entrar sem Login</Text>
+                    <Pressable>
+                        <Feather className="mx-20" name="edit-2" size={24} color="#04CBAC" />
                     </Pressable>
                 </View>
 
 
-                {/*CADASTRO*/}
-                <View className="ml-20 flex-row items-center mt-6">
-                    <Text className="m-4 font-semibold">Não tem Cadastro? </Text>
-                    <Pressable className="m-2" onPress={()=>navigation.navigate("Cadastro")} ><Text className="text-gray-500">Cadastre-se</Text></Pressable>
+                {/*FORMLÁRIO*/}
+                <View className="flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg ml-3 mr-3 mt-1 gap-2">
+                    <MaterialCommunityIcons style={{margin:7}} name="face-man-profile" size={25} color="#5a5a5a" />
+                    <TextInput
+                        className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
+                        placeholder="Nome Completo"
+                    />
                 </View>
+
+                <Text className="font-semibold text-2xl ml-4 m-3">Telefones</Text>
+                <View className="items-center">
+                    <Text className="text-gray-500 text-2xl">Nenhum telefone adicionado</Text>
+                </View>
+                
+
+                <View className="flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg m-3 gap-2">
+                    <MaterialCommunityIcons style={{margin:7}} name="phone-plus-outline" size={25} color="#5a5a5a" />
+                    <TextInput
+                        className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
+                        placeholder="Adicionar Telefone"
+                    />
+                </View>
+
+                <Text className="font-semibold text-2xl ml-4">Endereço</Text>
+                    <View className="w-5/5 flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg ml-3 m-3 gap-2">
+                        <FontAwesome5 style={{margin:7}} name="map-pin" size={25} color="#5a5a5a" />
+                        <TextInput
+                            className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
+                            placeholder="Logradouro"
+                        />
+                    </View>
+
+                <View className="flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg ml-3 m-3 gap-2">
+                    <Feather style={{margin:7}} name="map" size={25} color="#5a5a5a" />
+                    
+                    <TextInput
+                        className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
+                        placeholder="Complemento"
+                    />
+                </View>
+                
+                <View className="flex-row flex-wrap">
+                    <View className="w-1/4 flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg ml-3 m-3 gap-2">
+                        <SimpleLineIcons style={{margin:7}} name="location-pin" size={25} color="black" />
+                        <TextInput
+                            className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
+                            placeholder="CEP"
+                        />
+                    </View>
+
+                    <View className="w-3/5 flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg ml-3 m-3 gap-2">
+                        <Feather style={{margin:7}} name="mail" size={25} color="#5a5a5a" />
+                        <TextInput
+                            className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
+                            placeholder="Bairro"
+                        />
+                    </View>
+                </View>
+
+                <View className=" mb-7 flex-row items-center bg-gray-200 border-gray-300 border-2 rounded-lg ml-3 m-3 gap-2">
+                    <MaterialCommunityIcons style={{margin:7}} name="city-variant-outline" size={25} color="#5a5a5a" />
+                    <TextInput
+                        className="w-[90%] outline-none text-xl text-gray-500 placeholder:text-gray-500"
+                        placeholder="Cidade"
+                    />
+                </View>
+
+                <View className="items-center">
+                    <Picker
+                        className="w-[60%] p-1.5 text-xl bg-gray-200 border-gray-300 border-2 rounded-lg text-gray-500"
+                        selectedValue={selectedUf}
+                        onValueChange={(itemValue, itemIndex) =>
+                            setSelectedUf(itemValue)
+                        }>
+                        <Picker.Item label="AC" value="Pintura" />
+                        <Picker.Item label="AP" value="Teatro" />
+                        <Picker.Item label="AM" value="Música" />
+                        <Picker.Item label="PA" value="Literatura" />
+                    </Picker>
+                </View>
+
+                {/*SALVAR*/}
+                <View className="items-center">
+                    <Pressable style={{backgroundColor: "#04CBAC"}} className="mt-10 rounded-lg bg-emerald-500 p-2 w-3/5">
+                    <Text className="text-2xl text-white text-center">Salvar <Feather name="save" size={24} color="white" /></Text>
+                    </Pressable>
+                </View>
+
+
             </View>
         </View>
     );
