1ee6578577a4154dd7a4a3db426788eebdabf547
Lucas Pereira <lucaspereiravarjao83@gmail.com>
2026-03-09 10:43:56 -0300

    Feature de Modal de Configurações

--- src/pages/SeuPerfil/SeuPerfil.js
+++ src/pages/SeuPerfil/SeuPerfil.js
@@ -1,12 +1,21 @@
-import { Pressable, View, Text } from "react-native"
+import { Pressable, View, Text, Modal } from "react-native"
 import IconBar from "../../components/IconBar"
 import { useNavigation } from "@react-navigation/native"
 import Feather from '@expo/vector-icons/Feather';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import Post from "../../components/Post";
 import CompSeuPerfil from "../../components/CompSeuPerfil";
+import { useState } from 'react';
 
 export default function SeuPerfil() {
     const navigation = useNavigation();
+    const [modal, setModal] = useState (false);
+    const abrirModal = () =>{
+        setModal(true)
+    }
+    const fecharModal = () =>{
+        setModal(false)
+    }
     return (
         <View style={{flex:1}}>
             {/* HEADER */}
@@ -17,9 +26,34 @@ export default function SeuPerfil() {
                     </Pressable>
                 <Text className="text-2xl font-light text-white">Seu Perfil</Text>
                 </View>
-                <Pressable>
+                <Pressable onPress={abrirModal}>
                     <Feather name="settings" size={24} color="white" />
                 </Pressable>
+                <Modal
+                visible={modal}
+                transparent={true}>
+                    <View className="flex-1 justify-center items-center bg-black/50">
+                        <View className="bg-white rounded-3xl items-center w-[90%]">
+                            <View className="flex-row">
+                                <Pressable onPress={fecharModal}>
+                                    <Ionicons className="m-3" name="close" size={25} color="gray" />
+                                </Pressable>
+                                <Text className="text-2xl m-3 mr-14">Configurações</Text>
+                            </View>
+                            <Text className="text-xl m-3 text-gray-600">Preferência do app</Text>
+                            <Text className="text-xl m-3 text-gray-600">Acesso e segurança</Text>
+                            <Text className="text-xl m-3 text-gray-600">Suporte</Text>
+                            <Text className="text-xl m-3 text-gray-600">Contas bloqueadas</Text>
+                            <Text className="text-xl m-3 text-gray-600">Sobre o Art Connect</Text>
+                            <Pressable>
+                                <View className="flex-row">
+                                    <Text className="text-2xl m-3">Sair</Text>
+                                    <Ionicons className="m-4" name="exit-outline" size={25} color="gray" />
+                                </View>
+                            </Pressable>
+                        </View>
+                    </View>
+                </Modal>
             </View>
             {/* CONTEÚDO */}
             <View style={{flex:1}} className="p-2 gap-3">
